3d235bdab6484d75f2f2278dba3ca9c3e870e26d
jangkungpangestuaji <ajijangkung7@gmail.com>
2023-06-30 01:03:00 +0700

    Sampai Carousel

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,68 @@
 @extends('main.main')
+
+@section('content')
+<!-- Start Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="{{ url ('frontend/images/logo.png') }}" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+      <div class="d-flex">
+        <button class="btn btn-outline-success" type="submit">Log In</button>
+      </div>
+    </div>
+  </div>
+</nav>
+<!-- End Navbar -->
+
+<!-- Start Dashboard -->
+<div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="slide-1">
+        <img src="{{ url ('frontend/images/Slide1.png') }}" alt="" class="d-block w-100">
+        <div class="carousel-caption text-center caption-1">
+          <h2>BEST TO PLACE</h2>
+          <h1 class="place-title fw-bold">SUNGAI MUDAL</h1>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="slide-1">
+        <img src="{{ url ('frontend/images/Slide2.png') }}" alt="" class="d-block w-100">
+        <div class="carousel-caption text-center caption-1">
+          <h2>BEST TO PLACE</h2>
+          <h1 class="place-title fw-bold">KEMBANG SOKA</h1>
+        </div>
+      </div>
+    </div>
+    <!-- <div class="carousel-item">
+      <img src="{{ url ('frontend/images/Carousel.jpeg') }}" alt="...">
+    </div> -->
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<!-- End Dashboard -->
+@endsection()
--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -9,11 +9,14 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <!-- Local CSS -->
+    <link rel="stylesheet" href="{{ url ('frontend/style/main.css') }}">
+    <title>viewkulonprogo</title>
 </head>
 
 <body>
-    <h1>Hello, world!</h1>
+
+    @yield('content')
 
     <!-- Optional JavaScript; choose one of the two! -->
 
