45a081cbbfc4c672916963c662c3dd4cd4bf520c
jangkungpangestuaji <ajijangkung7@gmail.com>
2023-07-03 02:04:10 +0700

    Tambah

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,7 @@
 @extends('main.main')
-
+<style>
+  
+</style>
 @section('content')
 <!-- Start Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -65,4 +67,98 @@
   </button>
 </div>
 <!-- End Dashboard -->
+
+<!-- Start Maps -->
+<div class="container">
+  <h4 class="my-3 text-center">Tourist Spot Map</h4>
+  
+  <div class="mx-auto p-2 border" alt="Maps" style="width: 200px;">
+    
+  </div>
+</div>
+<!-- End Maps -->
+
+<!-- Start Most Searched-->
+<div class="container-fluid bg-light py-5 px-5 my-3">
+  <h4 class="py-3">Most searched tourist spots</h4>
+  <div class="row">
+    <div class="col-lg-6 my-2">
+      <h7 class="fw-bold">Sed ut perspiciatis</h7>
+      <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.</p>
+      <a href="#" class="btn btn-outline-primary">Read more</a>
+    </div>
+    <div class="col-lg-6 my-2">
+      <h7 class="fw-bold">Sed ut perspiciatis</h7>
+      <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.</p>
+      <a href="#" class="btn btn-outline-primary">Read more</a>
+    </div>
+    <div class="col-lg-6 my-2">
+      <h7 class="fw-bold">Sed ut perspiciatis</h7>
+      <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.</p>
+      <a href="#" class="btn btn-outline-primary">Read more</a>
+    </div>
+    <div class="col-lg-6 my-2">
+      <h7 class="fw-bold">Sed ut perspiciatis</h7>
+      <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.</p>
+      <a href="#" class="btn btn-outline-primary">Read more</a>
+    </div>
+  </div>
+</div>
+<!-- End Most Searched-->
+
+<!-- Start Information -->
+<div class="container w-50 my-5">
+  <div class="about text-center my-5">
+    <h6 class="fw-bold">About</h6>
+    <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.</p>
+  </div>
+  <div class="information text-center my-5">
+    <h6 class="fw-bold">More Information</h6>
+    <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.</p>
+  </div>
+  <div class="contact text-center my-5">
+    <h6 class="fw-bold">Contact</h6>
+    <div class="row mx-auto w-10">
+      <div class="col-3">
+        <a href="#">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1024px-Instagram_logo_2022.svg.png" width="50%" alt="">
+        </a>
+      </div>
+      <div class="col-3">
+        <a href="#">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1024px-Instagram_logo_2022.svg.png" width="50%" alt="">
+        </a>
+      </div>
+      <div class="col-3">
+        <a href="#">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1024px-Instagram_logo_2022.svg.png" width="50%" alt="">
+        </a>
+      </div>
+      <div class="col-3">
+        <a href="#">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1024px-Instagram_logo_2022.svg.png" width="50%" alt="">
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End Information -->
+
+<!-- Start Media Partner -->
+<div class="container-fluid text-center bg-light">
+  <h4 class="py-3">Media Partner</h4>
+  <h6 class="pb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</h6>
+  <div class="row">
+    <div class="col-3 mb-3"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1024px-Google_2015_logo.svg.png" alt="" width="50%"></div>
+    <div class="col-3 mb-3"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1024px-Google_2015_logo.svg.png" alt="" width="50%"></div>
+    <div class="col-3 mb-3"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1024px-Google_2015_logo.svg.png" alt="" width="50%"></div>
+    <div class="col-3 mb-3"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1024px-Google_2015_logo.svg.png" alt="" width="50%"></div>
+    <div class="col-3 mb-3"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1024px-Google_2015_logo.svg.png" alt="" width="50%"></div>
+    <div class="col-3 mb-3"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1024px-Google_2015_logo.svg.png" alt="" width="50%"></div>
+    <div class="col-3 mb-3"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1024px-Google_2015_logo.svg.png" alt="" width="50%"></div>
+    <div class="col-3 mb-3"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1024px-Google_2015_logo.svg.png" alt="" width="50%"></div>
+  </div>
+</div>
+<!-- End Media Partner -->
+
 @endsection()
